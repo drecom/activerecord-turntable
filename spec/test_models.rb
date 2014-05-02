5be48ae7212d4e9e081abe2ef3dcdc75295d5b59
gussan <egussan@gmail.com>
2014-05-02 20:03:58 +0900

    Follow behavior of sequencer dsl

--- spec/test_models.rb
+++ spec/test_models.rb
@@ -2,7 +2,7 @@
 class User < ActiveRecord::Base
   # shard by surrogate_key
   turntable :user_cluster, :id
-  sequencer
+  sequencer :user_seq_1
   has_one  :user_status
   has_many :cards_user
 end
@@ -10,7 +10,7 @@ end
 class UserStatus < ActiveRecord::Base
   # shard by other key
   turntable :user_cluster, :user_id
-  sequencer
+  sequencer :user_seq_1
   belongs_to :user
 end
 
@@ -20,7 +20,7 @@ end
 
 class CardsUser < ActiveRecord::Base
   turntable :user_cluster, :user_id
-  sequencer
+  sequencer :user_seq_1
 
   belongs_to :user
   belongs_to :card
