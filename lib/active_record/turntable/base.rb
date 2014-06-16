422d27934559cf3d27ce06a7d1b479784db61d36
gussan <egussan@gmail.com>
2014-06-16 15:24:37 +0900

    Fix ConnectionSpecification namespace

--- lib/active_record/turntable/base.rb
+++ lib/active_record/turntable/base.rb
@@ -74,7 +74,7 @@ module ActiveRecord::Turntable
           raise "Please install the #{config['adapter']} adapter: `gem install activerecord-#{config['adapter']}-adapter` (#{e})"
         end
         adapter_method = "#{config['adapter']}_connection"
-        ActiveRecord::Base::ConnectionSpecification.new(config, adapter_method)
+        ActiveRecord::ConnectionAdapters::ConnectionSpecification.new(config, adapter_method)
       end
 
       def clear_all_connections!
