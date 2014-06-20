d1cfa067e80527452a85459f24491f36d60d17d7
gussan <egussan@gmail.com>
2014-06-20 16:49:51 +0900

    Add parameter cluster name to sequencer DSL for using sequence without cluster

--- lib/active_record/turntable/base.rb
+++ lib/active_record/turntable/base.rb
@@ -50,7 +50,8 @@ module ActiveRecord::Turntable
 
       def force_connect_all_shards!
         conf = configurations[Rails.env]
-        shards = conf["shards"]
+        shards = []
+        shards += conf["shards"] if conf["shards"]
         shards = shards.merge(conf["seq"]) if conf["seq"]
         shards.each do |name, config|
           turntable_connections[name] ||=
--- lib/active_record/turntable/sequencer.rb
+++ lib/active_record/turntable/sequencer.rb
@@ -23,9 +23,9 @@ module ActiveRecord::Turntable
     @@tables = {}
     cattr_reader :sequences, :tables
 
-    def self.build(klass, sequence_name = nil)
-      sequence_name ||= current_cluster_config_for(klass)["seq"].keys.first
-      seq_config = current_cluster_config_for(klass)["seq"][sequence_name]
+    def self.build(klass, sequence_name = nil, cluster_name = nil)
+      sequence_name ||= current_cluster_config_for(cluster_name || klass)["seq"].keys.first
+      seq_config = current_cluster_config_for(cluster_name || klass)["seq"][sequence_name]
       seq_type = (seq_config["seq_type"] ? seq_config["seq_type"].to_sym : :mysql)
       @@tables[klass.table_name] ||= (@@sequences[sequence_name(klass.table_name, klass.primary_key)] ||= @@sequence_types[seq_type].new(klass, seq_config))
     end
@@ -52,8 +52,13 @@ module ActiveRecord::Turntable
 
     private
 
-    def self.current_cluster_config_for(klass)
-      ActiveRecord::Base.turntable_config["clusters"][klass.turntable_cluster_name.to_s]
+    def self.current_cluster_config_for(klass_or_name)
+      cluster_name = if klass_or_name.is_a?(Symbol)
+                       klass_or_name
+                     else
+                       klass_or_name.turntable_cluster_name.to_s
+                     end
+      ActiveRecord::Base.turntable_config["clusters"][cluster_name]
     end
   end
 end
