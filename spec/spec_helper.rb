99299c7597b51757fd9541876cb421b2e4b0c395
gussan <egussan@gmail.com>
2014-10-26 17:23:09 +0900

    Use new hash syntax

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -22,7 +22,7 @@ Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 RSpec.configure do |config|
   include TurntableHelper
 
-  config.filter_run :focus => true
+  config.filter_run focus: true
   config.run_all_when_everything_filtered = true
 
   config.before(:each) do
