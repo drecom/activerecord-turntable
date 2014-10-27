50eb4f2be0607c743e03a9d1e40532e538095a3e
gussan <egussan@gmail.com>
2014-10-27 11:39:53 +0900

    Update persistence patch for rails 4.1

--- lib/active_record/turntable/active_record_ext/persistence.rb
+++ lib/active_record/turntable/active_record_ext/persistence.rb
@@ -55,9 +55,7 @@ module ActiveRecord::Turntable::ActiveRecordExt
           finder_scope.where(primary_key => self[primary_key]).update_all(changes) == 1
         end
       end
-    end
 
-    ::ActiveRecord::Persistence.class_eval do
       private
 
       def relation_for_destroy
@@ -67,7 +65,7 @@ module ActiveRecord::Turntable::ActiveRecordExt
         klass      = self.class
 
         relation = self.class.unscoped.where(
-                                             self.class.arel_table[pk].eq(substitute))
+          self.class.arel_table[pk].eq(substitute))
         if klass.turntable_enabled? and klass.primary_key != klass.turntable_shard_key.to_s
           relation = relation.where(klass.turntable_shard_key => self.send(turntable_shard_key))
         end
@@ -76,28 +74,43 @@ module ActiveRecord::Turntable::ActiveRecordExt
         relation
       end
 
-      def update_record(attribute_names = @attributes.keys)
-        attributes_with_values = arel_attributes_with_values_for_update(attribute_names)
-        if attributes_with_values.empty?
-          0
-        else
+      if ActiveRecord::VERSION::STRING < "4.1"
+        def update_record(attribute_names = @attributes.keys)
+          attributes_with_values = arel_attributes_with_values_for_update(attribute_names)
+          if attributes_with_values.empty?
+            0
+          else
+            klass = self.class
+            column_hash = klass.connection.schema_cache.columns_hash klass.table_name
+            db_columns_with_values = attributes_with_values.map { |attr,value|
+              real_column = column_hash[attr.name]
+              [real_column, value]
+            }
+            bind_attrs = attributes_with_values.dup
+            bind_attrs.keys.each_with_index do |column, i|
+              real_column = db_columns_with_values[i].first
+              bind_attrs[column] = klass.connection.substitute_at(real_column, i)
+            end
+            condition_scope = klass.unscoped.where(klass.arel_table[klass.primary_key].eq(id_was || id))
+            if klass.turntable_enabled? and klass.primary_key != klass.turntable_shard_key.to_s
+              condition_scope = condition_scope.where(klass.turntable_shard_key => self.send(turntable_shard_key))
+            end
+            stmt = condition_scope.arel.compile_update(bind_attrs)
+            klass.connection.update stmt, 'SQL', db_columns_with_values
+          end
+        end
+      else
+        def update_record(attribute_names = @attributes.keys)
           klass = self.class
-          column_hash = klass.connection.schema_cache.columns_hash klass.table_name
-          db_columns_with_values = attributes_with_values.map { |attr,value|
-            real_column = column_hash[attr.name]
-            [real_column, value]
-          }
-          bind_attrs = attributes_with_values.dup
-          bind_attrs.keys.each_with_index do |column, i|
-            real_column = db_columns_with_values[i].first
-            bind_attrs[column] = klass.connection.substitute_at(real_column, i)
+          attributes_values = arel_attributes_with_values_for_update(attribute_names)
+          if attributes_values.empty?
+            0
+          else
+            scope = if klass.turntable_enabled? and klass.primary_key != klass.turntable_shard_key.to_s
+              klass.unscoped.where(klass.turntable_shard_key => self.send(turntable_shard_key))
+            end
+            klass.unscoped.update_record attributes_values, id, id_was, scope
           end
-          condition_scope = klass.unscoped.where(klass.arel_table[klass.primary_key].eq(id_was || id))
-          if klass.turntable_enabled? and klass.primary_key != klass.turntable_shard_key.to_s
-            condition_scope = condition_scope.where(klass.turntable_shard_key => self.send(turntable_shard_key))
-          end
-          stmt = condition_scope.arel.compile_update(bind_attrs)
-          klass.connection.update stmt, 'SQL', db_columns_with_values
         end
       end
     end
