f177a1ffd0dad2681bbe1d5f7fad1b3086239033
gussan <egussan@gmail.com>
2014-10-27 11:40:38 +0900

    Update log_subscriber patch for rails 4.1

--- lib/active_record/turntable/active_record_ext/log_subscriber.rb
+++ lib/active_record/turntable/active_record_ext/log_subscriber.rb
@@ -5,37 +5,35 @@ module ActiveRecord::Turntable
     module LogSubscriber
       extend ActiveSupport::Concern
 
-      included do
-        def sql(event)
-          self.class.runtime += event.duration
-          return unless logger.debug?
+      def sql_with_turntable(event)
+        self.class.runtime += event.duration
+        return unless logger.debug?
 
-          payload = event.payload
+        payload = event.payload
 
-          return if ActiveRecord::LogSubscriber::IGNORE_PAYLOAD_NAMES.include?(payload[:name])
+        return if ActiveRecord::LogSubscriber::IGNORE_PAYLOAD_NAMES.include?(payload[:name])
 
-          name  = "#{payload[:name]} (#{event.duration.round(1)}ms)"
-          shard = '[Shard: %s]' % (event.payload[:turntable_shard_name] ? event.payload[:turntable_shard_name] : nil)
-          sql   = payload[:sql].squeeze(' ')
-          binds = nil
+        name  = "#{payload[:name]} (#{event.duration.round(1)}ms)"
+        shard = '[Shard: %s]' % (event.payload[:turntable_shard_name] ? event.payload[:turntable_shard_name] : nil)
+        sql   = payload[:sql].squeeze(' ')
+        binds = nil
 
-          unless (payload[:binds] || []).empty?
-            binds = "  " + payload[:binds].map { |col,v|
-              render_bind(col, v)
-            }.inspect
-          end
-
-          if odd?
-            name = color(name, ActiveRecord::LogSubscriber::CYAN, true)
-            shard = color(shard, ActiveRecord::LogSubscriber::CYAN, true)
-            sql  = color(sql, nil, true)
-          else
-            name = color(name, ActiveRecord::LogSubscriber::MAGENTA, true)
-            shard = color(shard, ActiveRecord::LogSubscriber::MAGENTA, true)
-          end
-
-          debug "  #{name} #{shard} #{sql}#{binds}"
+        unless (payload[:binds] || []).empty?
+          binds = "  " + payload[:binds].map { |col,v|
+            render_bind(col, v)
+          }.inspect
         end
+
+        if odd?
+          name = color(name, ActiveRecord::LogSubscriber::CYAN, true)
+          shard = color(shard, ActiveRecord::LogSubscriber::CYAN, true)
+          sql  = color(sql, nil, true)
+        else
+          name = color(name, ActiveRecord::LogSubscriber::MAGENTA, true)
+          shard = color(shard, ActiveRecord::LogSubscriber::MAGENTA, true)
+        end
+
+        debug "  #{name} #{shard} #{sql}#{binds}"
       end
     end
   end
