c273a3dd7f0ae3d2b31c1dbc89ce1352d25945d2
gussan <egussan@gmail.com>
2014-11-20 11:12:48 +0900

    Fix not to destroy other database extension's connection proxy object

--- lib/active_record/turntable/active_record_ext/connection_handler_extension.rb
+++ lib/active_record/turntable/active_record_ext/connection_handler_extension.rb
@@ -13,14 +13,14 @@ module ActiveRecord::Turntable
       def pool_for_with_turntable(owner)
         owner_to_pool.fetch(owner.name) {
           if ancestor_pool = pool_from_any_process_for(owner)
-            if ancestor_pool.is_a?(ActiveRecord::Turntable::PoolProxy)
-              # Use same PoolProxy object
-              owner_to_pool[owner.name] = ancestor_pool
-            else
+            if ancestor_pool.is_a?(ActiveRecord::ConnectionAdapters::ConnectionPool)
               # A connection was established in an ancestor process that must have
               # subsequently forked. We can't reuse the connection, but we can copy
               # the specification and establish a new connection with it.
               establish_connection owner, ancestor_pool.spec
+            else
+              # Use same PoolProxy object
+              owner_to_pool[owner.name] = ancestor_pool
             end
           else
             owner_to_pool[owner.name] = nil
