2c7599ccab0ab4cb6029da51068c6ac799d092d2
gussan <egussan@gmail.com>
2015-01-19 16:00:35 +0900

    Skip initializing clusters if  is not found

--- lib/active_record/turntable/railtie.rb
+++ lib/active_record/turntable/railtie.rb
@@ -15,7 +15,11 @@ module ActiveRecord::Turntable
     # initialize
     initializer "turntable.initialize_clusters" do
       ActiveSupport.on_load(:active_record) do
-        ActiveRecord::Turntable::Config.load!
+        if File.exist?(ActiveRecord::Base.turntable_config_file)
+          ActiveRecord::Turntable::Config.load!
+        else
+          warn("[activerecord-turntable] config/turntable.yml is not found. skipped initliazing cluster.")
+        end
       end
     end
 
