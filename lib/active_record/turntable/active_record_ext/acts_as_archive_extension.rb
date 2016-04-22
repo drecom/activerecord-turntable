946e6992a5ae005e1fbfd93921814a7b2b273648
gussan <egussan@gmail.com>
2016-04-22 19:18:51 +0900

    Remove alias_method_chain from acts_as_archive extension

--- lib/active_record/turntable/active_record_ext/acts_as_archive_extension.rb
+++ lib/active_record/turntable/active_record_ext/acts_as_archive_extension.rb
@@ -1,22 +1,31 @@
-begin
-  require "acts_as_archive"
-  # acts_as_archive extension
-  class ActsAsArchive
-    class << self
-      # @note use the same shard which `from` shard using
-      def move_with_turntable(config, where, merge_options = {})
-        if [config[:to], config[:from]].all? { |k| k.try(:turntable_enabled?) }
-          current_shard = config[:from].connection.current_shard.name.to_sym
-          config[:to].connection.with_shard(current_shard) {
-            move_without_turntable(config, where, merge_options)
-          }
-        else
-          move_without_turntable(config, where, merge_options)
+module ActiveRecord::Turntable
+  module ActiveRecordExt
+    module ActsAsArchiveExt
+      def self.prepended(base)
+        class << base
+          prepend ClassMethods
         end
       end
 
-      alias_method_chain :move, :turntable
+      module ClassMethods
+        # @note use the same shard which `from` shard using
+        def move(config, where, merge_options = {})
+          if [config[:to], config[:from]].all? { |k| k.try(:turntable_enabled?) }
+            current_shard = config[:from].connection.current_shard.name.to_sym
+            config[:to].connection.with_shard(current_shard) {
+              super
+            }
+          else
+            super
+          end
+        end
+      end
+    end
+
+    begin
+      require 'acts_as_archive'
+      ActsAsArchive.prepend ActsAsArchiveExt
+    rescue LoadError
     end
   end
-rescue LoadError
 end
