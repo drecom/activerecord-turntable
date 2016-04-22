74a92b813fa43b455c2629578c69ce585879bd13
gussan <egussan@gmail.com>
2016-04-22 19:00:54 +0900

    Remove alias_method_chain from ar-import extension

--- lib/active_record/turntable/active_record_ext/activerecord_import_ext.rb
+++ lib/active_record/turntable/active_record_ext/activerecord_import_ext.rb
@@ -2,17 +2,12 @@ module ActiveRecord::Turntable
   module ActiveRecordExt
     # activerecord-import extension
     module ActiverecordImportExt
-      extend ActiveSupport::Concern
-
-      included do
-        alias_method_chain :values_sql_for_columns_and_attributes, :turntable
-      end
 
       private
 
         # @note override for sequencer injection
         # @see https://github.com/zdennis/activerecord-import/blob/ba909fed5a4785fe9c7cce89e48e1242bb6804ea/lib/activerecord-import/import.rb#L558-L581
-        def values_sql_for_columns_and_attributes_with_turntable(columns, array_of_attributes)
+        def values_sql_for_columns_and_attributes(columns, array_of_attributes)
           connection_memo = connection
           array_of_attributes.map do |arr|
             my_values = arr.each_with_index.map do |val, j|
@@ -41,7 +36,7 @@ module ActiveRecord::Turntable
     begin
       require "activerecord-import"
       require "activerecord-import/base"
-      (class << ActiveRecord::Base; self; end).include(ActiverecordImportExt)
+      (class << ActiveRecord::Base; self; end).prepend(ActiverecordImportExt)
     rescue LoadError
     end
   end
