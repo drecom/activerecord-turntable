2ad357dab40aedde25d12838e208f56442f9f555
gussan <egussan@gmail.com>
2016-10-09 23:58:32 +0900

    Follow LogSubscriber changes and clean up

--- lib/active_record/turntable/active_record_ext/log_subscriber.rb
+++ lib/active_record/turntable/active_record_ext/log_subscriber.rb
@@ -3,40 +3,14 @@ require "active_record/log_subscriber"
 module ActiveRecord::Turntable
   module ActiveRecordExt
     module LogSubscriber
-
-      protected
-
-        # @note Override to add shard name logging
-        def sql(event)
-          self.class.runtime += event.duration
-          return unless logger.debug?
-
-          payload = event.payload
-
-          return if ActiveRecord::LogSubscriber::IGNORE_PAYLOAD_NAMES.include?(payload[:name])
-
-          name  = "#{payload[:name]} (#{event.duration.round(1)}ms)"
-          shard = "[Shard: %s]" % (event.payload[:turntable_shard_name] ? event.payload[:turntable_shard_name] : nil)
-          sql   = payload[:sql].squeeze(" ")
-          binds = nil
-
-          unless (payload[:binds] || []).empty?
-            binds = "  " + payload[:binds].map { |col, v|
-              render_bind(col, v)
-            }.inspect
-          end
-
-          if odd?
-            name = color(name, ActiveRecord::LogSubscriber::CYAN, true)
-            shard = color(shard, ActiveRecord::LogSubscriber::CYAN, true)
-            sql  = color(sql, nil, true)
-          else
-            name = color(name, ActiveRecord::LogSubscriber::MAGENTA, true)
-            shard = color(shard, ActiveRecord::LogSubscriber::MAGENTA, true)
-          end
-
-          debug "  #{name} #{shard} #{sql}#{binds}"
+      # @note prepend to add shard name logging
+      def sql(event)
+        payload = event.payload
+        if payload[:turntable_shard_name]
+          payload[:name] = "#{payload[:name]} [Shard: #{payload[:turntable_shard_name]}]"
         end
+        super
+      end
     end
   end
 end
