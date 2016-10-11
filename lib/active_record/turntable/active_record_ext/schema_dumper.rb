76fa214ba0496e021e8b69d02e384e9cfcdd0055
gussan <egussan@gmail.com>
2016-10-11 18:28:32 +0900

    Update SchemaDumper patch

--- lib/active_record/turntable/active_record_ext/schema_dumper.rb
+++ lib/active_record/turntable/active_record_ext/schema_dumper.rb
@@ -12,34 +12,50 @@ module ActiveRecord::Turntable
             tbl = StringIO.new
 
             # first dump primary key column
-            pk = @connection.primary_key(table)
+            if @connection.respond_to?(:primary_keys)
+              pk = @connection.primary_keys(table)
+              pk = pk.first unless pk.size > 1
+            else
+              pk = @connection.primary_key(table)
+            end
 
             tbl.print = if table =~ /\A(.*)_id_seq\z/
-                          "  create_sequence_for #{remove_prefix_and_suffix($1).inspect}"
+                          "  create_sequence_for #{remove_prefix_and_suffix(Regexp.last_match(1)).inspect}"
                         else
                           "  create_table #{remove_prefix_and_suffix(table).inspect}"
                         end
-            pkcol = columns.detect { |c| c.name == pk }
-            if pkcol
-              if pk != 'id'
-                tbl.print %Q(, primary_key: "#{pk}")
-              elsif pkcol.sql_type == 'bigint'
-                tbl.print ", id: :bigserial"
-              elsif pkcol.sql_type == 'uuid'
-                tbl.print ", id: :uuid"
-                tbl.print %Q(, default: #{pkcol.default_function.inspect})
+
+            case pk
+            when String
+              tbl.print ", primary_key: #{pk.inspect}" unless pk == 'id'
+              pkcol = columns.detect { |c| c.name == pk }
+              pkcolspec = @connection.column_spec_for_primary_key(pkcol)
+              if pkcolspec.present?
+                pkcolspec.each do |key, value|
+                  tbl.print ", #{key}: #{value}"
+                end
               end
+            when Array
+              tbl.print ", primary_key: #{pk.inspect}"
             else
               tbl.print ", id: false"
             end
             tbl.print ", force: :cascade"
+
+            table_options = @connection.table_options(table)
+            tbl.print ", options: #{table_options.inspect}" unless table_options.blank?
+
+            if comment = @connection.table_comment(table).presence
+              tbl.print ", comment: #{comment.inspect}"
+            end
+
             tbl.puts " do |t|"
 
             # then dump all non-primary key columns
             column_specs = columns.map do |column|
               raise StandardError, "Unknown type '#{column.sql_type}' for column '#{column.name}'" unless @connection.valid_type?(column.type)
               next if column.name == pk
-              @connection.column_spec(column, @types)
+              @connection.column_spec(column)
             end.compact
 
             # find all migration keys used in this table
@@ -53,27 +69,27 @@ module ActiveRecord::Turntable
             }
 
             # the string we're going to sprintf our values against, with standardized column widths
-            format_string = lengths.map { |len| "%-#{len}s" }
+            format_string = lengths.map{ |len| "%-#{len}s" }
 
             # find the max length for the 'type' column, which is special
-            type_length = column_specs.map { |column| column[:type].length }.max
+            type_length = column_specs.map{ |column| column[:type].length }.max
 
             # add column type definition to our format string
             format_string.unshift "    t.%-#{type_length}s "
 
-            format_string *= ""
+            format_string *= ''
 
             column_specs.each do |colspec|
-              values = keys.zip(lengths).map { |key, len| colspec.key?(key) ? colspec[key] + ", " : " " * len }
+              values = keys.zip(lengths).map{ |key, len| colspec.key?(key) ? colspec[key] + ", " : " " * len }
               values.unshift colspec[:type]
-              tbl.print((format_string % values).gsub(/,\s*$/, ""))
+              tbl.print((format_string % values).gsub(/,\s*$/, ''))
               tbl.puts
             end
 
+            indexes_in_create(table, tbl)
+
             tbl.puts "  end"
             tbl.puts
-
-            indexes(table, tbl)
 
             tbl.rewind
             stream.print tbl.read
