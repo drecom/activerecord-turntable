6f9abd8cfb19f8acde97a39f508a012662e02a22
misoobu <misoobu@me.com>
2017-01-22 00:56:56 +0900

    Follow the activerecord-import
    This changes include a performance improvement:
    https://github.com/zdennis/activerecord-import/commit/4b1e84308c5eecafd9c90b3df6c909bfb106e125

--- lib/active_record/turntable/active_record_ext/activerecord_import_ext.rb
+++ lib/active_record/turntable/active_record_ext/activerecord_import_ext.rb
@@ -3,9 +3,11 @@ module ActiveRecord::Turntable
     # activerecord-import extension
     module ActiverecordImportExt
       # @note override for sequencer injection
-      # @see https://github.com/zdennis/activerecord-import/blob/ba909fed5a4785fe9c7cce89e48e1242bb6804ea/lib/activerecord-import/import.rb#L558-L581
+      # @see https://github.com/zdennis/activerecord-import/blob/b325ebb644160a09db6e269e414f33561cb21272/lib/activerecord-import/import.rb#L661-L689
       private def values_sql_for_columns_and_attributes(columns, array_of_attributes)
         connection_memo = connection
+        type_caster_memo = type_caster if respond_to?(:type_caster)
+
         array_of_attributes.map do |arr|
           my_values = arr.each_with_index.map do |val, j|
             column = columns[j]
@@ -18,8 +20,8 @@ module ActiveRecord::Turntable
                 connection_memo.next_value_for_sequence(sequence_name)
               end
             elsif column
-              if respond_to?(:type_caster) && type_caster.respond_to?(:type_cast_for_database) # Rails 5.0 and higher
-                connection_memo.quote(type_caster.type_cast_for_database(column.name, val))
+              if defined?(type_caster_memo) && type_caster_memo.respond_to?(:type_cast_for_database) # Rails 5.0 and higher
+                connection_memo.quote(type_caster_memo.type_cast_for_database(column.name, val))
               else # Rails 4.2 and higher
                 connection_memo.quote(column.type_cast_from_user(val), column)
               end
