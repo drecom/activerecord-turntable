79964fd5f28904a3958e1dc569687ff8a5493663
gussan <egussan@gmail.com>
2017-02-08 14:37:16 +0900

    Use DEFAULT_ENV instead of `Rails.env`
    In case of rails is not required

--- lib/active_record/turntable/cluster_helper_methods.rb
+++ lib/active_record/turntable/cluster_helper_methods.rb
@@ -30,7 +30,8 @@ module ActiveRecord::Turntable
       end
 
       def force_connect_all_shards!
-        conf = configurations[Rails.env]
+        conf = configurations[::ActiveRecord::ConnectionHandling::DEFAULT_ENV.call.to_sym]
+
         shards = HashWithIndifferentAccess.new
         shards = shards.merge(conf[:shards]) if conf[:shards]
         shards = shards.merge(conf[:seq]) if conf[:seq]
