01f905ea2f40f43abbee96c0a4ce911276110d9c
gussan <egussan@gmail.com>
2017-03-23 11:12:54 +0900

    Update AbstractAdapter extension to follow 5.1 changes

--- lib/active_record/turntable/active_record_ext/abstract_adapter.rb
+++ lib/active_record/turntable/active_record_ext/abstract_adapter.rb
@@ -15,8 +15,26 @@ module ActiveRecord::Turntable
 
       # @note override for append current shard name
       # rubocop:disable Style/HashSyntax, Style/MultilineMethodCallBraceLayout
-      if ActiveRecord::Turntable::Util.ar_version_equals_or_later?("5.0.3")
+      if ActiveRecord::Turntable::Util.ar51_or_later?
         def log(sql, name = "SQL", binds = [], type_casted_binds = [], statement_name = nil)
+          @instrumenter.instrument(
+            "sql.active_record",
+            sql:                  sql,
+            name:                 name,
+            binds:                binds,
+            type_casted_binds:    type_casted_binds,
+            statement_name:       statement_name,
+            connection_id:        object_id,
+            turntable_shard_name: turntable_shard_name) do
+              @lock.synchronize do
+                yield
+              end
+            end
+        rescue => e
+          raise translate_exception_class(e, sql)
+        end
+      elsif ActiveRecord::Turntable::Util.ar_version_equals_or_later?("5.0.3")
+        def log(sql, name = "SQL", binds = [], type_casted_binds = [], statement_name = nil) # :doc:
           @instrumenter.instrument(
             "sql.active_record",
             sql:                  sql,
