5e31def016ba10610cea45ed39c425e41ea7c0f3
gussan <egussan@gmail.com>
2017-05-29 14:43:31 +0900

    Fix LogSubscriber log format
    This commit revert log format to be the same as activerecord-turntable 2.x and
    fix colorizing as original AR::LogSubscriber.
    
    before:
    
      SQL [Shard: shard_1] (0.0ms) XXXX
    
    after:
    
      SQL (0.0ms) [Shard: shard_1] XXXX

--- lib/active_record/turntable/active_record_ext/log_subscriber.rb
+++ lib/active_record/turntable/active_record_ext/log_subscriber.rb
@@ -5,17 +5,30 @@ module ActiveRecord::Turntable
     module LogSubscriber
       # @note prepend to add shard name logging
       def sql(event)
+        self.class.runtime += event.duration
+        return unless logger.debug?
+
         payload = event.payload
 
-        if self.class::IGNORE_PAYLOAD_NAMES.include?(payload[:name])
-          self.class.runtime += event.duration
-          return
+        return if ActiveRecord::LogSubscriber::IGNORE_PAYLOAD_NAMES.include?(payload[:name])
+
+        name  = "#{payload[:name]} (#{event.duration.round(1)}ms)"
+        name  = "#{name} [Shard: #{payload[:turntable_shard_name]}]" if payload[:turntable_shard_name]
+        name  = "CACHE #{name}" if payload[:cached]
+        sql   = payload[:sql]
+        binds = nil
+
+        unless (payload[:binds] || []).empty?
+          casted_params = type_casted_binds(payload[:binds], payload[:type_casted_binds])
+          binds = "  " + payload[:binds].zip(casted_params).map { |attr, value|
+            render_bind(attr, value)
+          }.inspect
         end
 
-        if payload[:turntable_shard_name]
-          payload[:name] = "#{payload[:name]} [Shard: #{payload[:turntable_shard_name]}]"
-        end
-        super
+        name = colorize_payload_name(name, payload[:name])
+        sql  = color(sql, sql_color(sql), true)
+
+        debug "  #{name}  #{sql}#{binds}"
       end
     end
   end
