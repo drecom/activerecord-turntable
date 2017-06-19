c8cf7b6f2e9f70c4e8cdce8cd4c4b9c97cdc93e6
gussan <egussan@gmail.com>
2017-06-19 17:01:52 +0900

    Use factory_girl on locking_optimistic spec

--- spec/active_record/turntable/active_record_ext/locking_optimistic_spec.rb
+++ spec/active_record/turntable/active_record_ext/locking_optimistic_spec.rb
@@ -9,15 +9,12 @@ describe ActiveRecord::Turntable::ActiveRecordExt::LockingOptimistic do
     ActiveRecord::Base.turntable_config.instance_variable_get(:@config)[:raise_on_not_specified_shard_update] = false
   end
 
-  let!(:user_status) do
-    user_status = UserStatus.new(user_id: 1)
-    user_status.id = 10
-    user_status.save
-    user_status
-  end
+  let(:user) { create(:user) }
+  let(:user_status) { user.user_status }
 
   describe "optimistic locking" do
     subject { user_status.update_attributes(hp: 20) }
+
     it { expect { subject }.to change(user_status, :lock_version).by(1) }
   end
 
@@ -26,7 +23,9 @@ describe ActiveRecord::Turntable::ActiveRecordExt::LockingOptimistic do
       user_status.update_attributes(data: { foo: "bar" })
       user_status.reload
     end
+
     subject { user_status.data }
+
     it { expect { subject }.not_to raise_error }
   end
 end
