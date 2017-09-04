a374405ebd02a63a7d54343ac8b08bbf71eb577b
gussan <egussan@gmail.com>
2017-09-04 14:45:38 +0900

    Remove unused methods

--- lib/active_record/turntable/sequencer.rb
+++ lib/active_record/turntable/sequencer.rb
@@ -1,8 +1,3 @@
-# -*- coding: utf-8 -*-
-#
-# 採番
-#
-
 module ActiveRecord::Turntable
   class Sequencer
     extend ActiveSupport::Autoload
@@ -13,18 +8,6 @@ module ActiveRecord::Turntable
       autoload :Barrage
     end
 
-    class_attribute :sequence_types
-    class_attribute :sequences
-    class_attribute :tables
-
-    self.sequence_types = {
-      api: Api,
-      mysql: Mysql,
-      barrage: Barrage,
-    }
-    self.sequences = {}
-    self.tables = {}
-
     def sequence_name(table_name, primary_key = 'id')
       [table_name, primary_key, "seq"].join("_")
     end
@@ -34,13 +17,6 @@ module ActiveRecord::Turntable
     end
 
     class << self
-      def build(klass, sequence_name = nil, cluster_name = nil)
-        sequence_name ||= current_cluster_config_for(cluster_name || klass).sequencers.first
-        seq_config = current_cluster_config_for(cluster_name || klass)[:seq][sequence_name]
-        seq_type = (seq_config[:seq_type] ? seq_config[:seq_type].to_sym : :mysql)
-        tables[klass.table_name] ||= (sequences[sequence_name(klass.table_name, klass.primary_key)] ||= sequence_types[seq_type].new(klass, seq_config))
-      end
-
       def class_for(name_or_class)
         case name_or_class
         when Sequencer
@@ -50,28 +26,9 @@ module ActiveRecord::Turntable
         end
       end
 
-      def has_sequencer?(table_name)
-        !!tables[table_name]
-      end
-
       def sequence_name(table_name, primary_key = 'id')
         [table_name, primary_key, "seq"].join("_")
       end
-
-      def table_name(seq_name)
-        seq_name.split("_").first
-      end
-
-      private
-
-        def current_cluster_config_for(klass_or_name)
-          cluster_name = if klass_or_name.is_a?(Symbol)
-                           klass_or_name
-                         else
-                           klass_or_name.turntable_cluster_name.to_s
-                         end
-          ActiveRecord::Base.turntable_configuration.cluster(cluster_name)
-        end
     end
 
     def next_sequence_value(sequence_name)
