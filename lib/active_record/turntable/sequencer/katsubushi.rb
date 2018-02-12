87a54d9dd088cd8413094c308cdfb762ff06177c
gussan <egussan@gmail.com>
2018-02-12 23:30:05 +0900

    Randomize keys for load balancing

--- lib/active_record/turntable/sequencer/katsubushi.rb
+++ lib/active_record/turntable/sequencer/katsubushi.rb
@@ -10,12 +10,13 @@ module ActiveRecord::Turntable
         end
 
         require "dalli"
+        require "securerandom"
         dalli_opts = opts.with_indifferent_access
         @client = Dalli::Client.new(servers, dalli_opts)
       end
 
-      def next_sequence_value(sequence_name)
-        @client.get(sequence_name || "id").to_i
+      def next_sequence_value(*)
+        @client.get(SecureRandom.hex(4)).to_i
       end
 
       def current_sequence_value(sequence_name)
