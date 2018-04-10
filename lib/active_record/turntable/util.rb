d7e505f36d39fbc0cb2ef63c9095db0677216a9f
gussan <egussan@gmail.com>
2018-04-10 14:14:25 +0900

    Follow rails 5.0.7 changes

--- lib/active_record/turntable/util.rb
+++ lib/active_record/turntable/util.rb
@@ -12,6 +12,13 @@ module ActiveRecord::Turntable
       ActiveRecord.gem_version.release
     end
 
+    def ar_version_satisfy?(requirement)
+      unless requirement.is_a?(Gem::Requirement)
+        requirement = Gem::Requirement.new(requirement)
+      end
+      requirement.satisfied_by?(ar_version)
+    end
+
     def ar51_or_later?
       ar_version_equals_or_later?("5.1")
     end
@@ -19,6 +26,7 @@ module ActiveRecord::Turntable
     module_function :ar_version_equals_or_later?,
                     :ar_version_earlier_than?,
                     :ar_version,
+                    :ar_version_satisfy?,
                     :ar51_or_later?
   end
 end
