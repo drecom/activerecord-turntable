3b4114bb0b50840f27d6f8529c07c150b586aae2
gussan <egussan@gmail.com>
2018-06-04 15:14:33 +0900

    Fix to support activerecord-import

---
+++ spec/active_record/turntable/active_record_ext/activerecord_import_ext_spec.rb
@@ -0,0 +1,28 @@
+require "spec_helper"
+require "active_record/turntable/active_record_ext/activerecord_import_ext"
+
+describe ActiveRecord::Turntable::ActiveRecordExt::ActiverecordImportExt do
+  context "With sequencer enabled model" do
+    subject { -> { UserItem.import(rows) } }
+
+    let(:rows) do
+      [
+        build(:user_item, user: create(:user, :in_shard1)),
+        build(:user_item, user: create(:user, :in_shard2)),
+      ]
+    end
+
+    it { is_expected.not_to raise_error }
+    it "creates one record on shard_1" do
+      subject.call
+      item_count = UserItem.with_shard(1) { UserItem.count }
+      expect(item_count).to eq(1)
+    end
+
+    it "creates one record on shard_2" do
+      subject.call
+      item_count = UserItem.with_shard(20001) { UserItem.count }
+      expect(item_count).to eq(1)
+    end
+  end
+end
